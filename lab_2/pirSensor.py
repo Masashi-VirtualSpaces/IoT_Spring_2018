338d9a9d270caa959b2798603eb416377b62e3e4
Masashi Schafer <masashi@virtualspaces.io>
2018-04-16 11:43:02 -0700

    added code for lab2

--- lab_2/pirSensor.py
+++ lab_2/pirSensor.py
@@ -8,6 +8,9 @@ GPIO.setmode(GPIO.BOARD)
 CONST_INPUT = 3
 CONST_OUTPUT = 11
 
+GPIO.setmode(CONST_OUTPUT,GPIO.OUT,initial=GPIO.HIGH)
+GPIO.setmode(CONST_INPUT,GPIO.IN)
+
 GPIO.output(CONST_OUTPUT,True)
 GPIO.input(CONST_INPUT)
 #Define pin 3 as an output pin
