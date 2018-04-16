e3f9c5172e0367460a68851458b9963238c29be1
Mahekdeep Singh <mahekdeep@Mahekdeeps-MacBook-Pro.local>
2018-04-16 12:30:00 -0700

    added ThingSpeak

--- lab_2/pirSensor.py
+++ lab_2/pirSensor.py
@@ -10,15 +10,7 @@ key = 'A86H9XNAIHMD6FC8' #write APIkey from ThingSpeak
 params = urllib.urlencode({'field8':sensor, 'key':key}) #Parameters channel field and data sent
 headers = {"Content-typZZe":"application/x-www-form-    urlencoded","Accept":"text/plain"}
 conn = httplib.HTTPConnection("api.thingspeak.com:80")
-try: #Sending to ThingSpeak
-    conn.request("POST", "/update", params, headers)
-    response = conn.getresponse()
-    print (sensor)
-    #print (response.status, response.reason)
-    data = response.read()
-    conn.close()
-except:
-    print ("connection failed")
+
 
 
 
@@ -49,3 +41,13 @@ while True:
         GPIO.output(CONST_OUTPUT,True)
         #Turns on the LED
         time.sleep(1.5)
+
+    try: #Sending to ThingSpeak
+        conn.request("POST", "/update", params, headers)
+        response = conn.getresponse()
+        print (sensor)
+        #print (response.status, response.reason)
+        data = response.read()
+        conn.close()
+    except:
+        print ("connection failed")
