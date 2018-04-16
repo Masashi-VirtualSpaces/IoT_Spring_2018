0cbb7f194ed7c3a68c972f8d62377a212c8c9f40
Masashi Schafer <masashi@virtualspaces.io>
2018-04-16 11:37:54 -0700

    added code for lab2

--- lab_2/pirSensor.py
+++ lab_2/pirSensor.py
@@ -1,1 +1,27 @@
 #4/16/17
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
+CONST_INPUT = 3
+CONST_OUTPUT = 11
+
+GPIO.output(CONST_OUTPUT)
+GPIO.input(CONST_INPUT)
+#Define pin 3 as an output pin
+#Define pin 11 as an input pin
+
+while True:
+        sensor = GPIO.input(CONST_INPUT)
+        if (sensor == True):
+            print ("No intruders", sensor)
+            #Turns off the LED
+
+        time.sleep(1.5)
+
+    elif (sensor ==  False):
+            print ("Intruder detected", sensor)
+            #Turns on the LED
+            time.sleep(1.5)
